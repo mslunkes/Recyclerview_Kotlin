47209edaac66cb9702431c1fefe612181f7b9f51
Matias Scherer Lunkes <mslunkes@outlook.com>
2024-05-02 18:26:44 -0300

    Create Grid Layout

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.devspace.recyclerview
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -26,11 +28,21 @@ class MainActivity : AppCompatActivity() {
 
 
         val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val ivList = findViewById<ImageView>(R.id.iv_list)
+        val ivGrid = findViewById<ImageView>(R.id.iv_grid)
         val adapter = ContactListAdapter()
 
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
         adapter.submitList(contacts)
+
+        ivGrid.setOnClickListener {
+            rvList.layoutManager = GridLayoutManager(this, 2)
+        }
+
+        ivList.setOnClickListener{
+            rvList.layoutManager = LinearLayoutManager(this)
+        }
     }
 }
 
